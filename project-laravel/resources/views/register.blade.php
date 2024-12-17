9f4bdee625efe5f70871138c6837eaaaa784e038
yanFajR <imryanfajri@gmail.com>
2024-12-17 23:16:30 +0700

    Tugas-12

---
+++ project-laravel/resources/views/register.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas - Hari 1</title>
+</head>
+<body>
+    <h1>Buat Account Baru!</h1>
+    <h3>Sign Up Form</h3>
+    <form action="/welcome" method="POST">
+        @csrf
+        <label for="firstName">First Name:</label><br>
+        <input type="text" name="firstName"><br><br>
+        <label for="lastName">Last Name:</label><br>
+        <input type="text" name="lastName"><br><br>
+        <label>Gender:</label><br>
+        <input type="radio" name="gender" value="male">Male<br>
+        <input type="radio" name="gender" value="female">Female<br>
+        <input type="radio" name="gender" value="other">Other<br><br>
+        <label for="">Nationality:</label><br>
+        <select name="Nationality">
+            <option value="Indonesian" name="nationality">Indonesian</option>
+            <option value="Singaporean" name="nationality">Singaporean</option>
+            <option value="Malaysian" name="nationality">Malaysian</option>
+        </select><br><br>
+        <label>Languange Spoken:</label><br>
+        <input type="checkbox" name="language" value="Indonesia">Bahasa Indonesia<br>
+        <input type="checkbox" name="language" value="English">English<br>
+        <input type="checkbox" name="language" value="Other">Other<br><br>
+        <label for="bio">Bio:</label><br>
+        <textarea name="bio" id="bio" rows="10" cols="30"></textarea><br>
+        <input type="submit" value="Sign Up">
+    </form>
+</body>
+</html>
---
+++ project-laravel/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas - Hari 1</title>
+</head>
+<body>
+    <h1>SELAMAT DATANG {{ $firstName }} {{ $lastName }}!</h1>
+    <H3>Terima kasih telah bergabung di Sanberbook. Social Media kita bersama!</H3>
+</body>
+</html>
